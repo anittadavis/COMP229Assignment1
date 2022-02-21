2408b83e7ecbd866517acfe9947373228a3fff5d
anittadavis <anittadavis99@gmail.com>
2022-02-21 13:56:17 -0500

    update db uri

--- server/config/db.js
+++ server/config/db.js
@@ -1,5 +1,5 @@
 module.exports = 
 {
-   "URI": "mongodb+srv://Anitta99:Anitta%4099@portfolio.nyn2l.mongodb.net/Portfolio"
+   "URI": "mongodb+srv://Anitta99:gmSz0zfhFV4OTunK@portfolio.nyn2l.mongodb.net/Portfolio?retryWrites=true&w=majority"
 }
 
